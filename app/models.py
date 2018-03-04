8f1aec0d338d8db60f7747eb0ca6fc71c29f9c44
Ramone McPherson <ramonemcpherson@gmail.com>
2018-03-04 13:46:28 +0000

    created migration file

--- app/models.py
+++ app/models.py
@@ -7,6 +7,11 @@ class UserProfile(db.Model):
     last_name = db.Column(db.String(80))
     username = db.Column(db.String(80), unique=True)
 
+    def __init__(self,first_name,last_name,username):
+        self.username=username
+        self.first_name=first_name
+        self.last_name=last_name
+    
     def is_authenticated(self):
         return True
 
